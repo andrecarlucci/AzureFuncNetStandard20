52119af846d3168094c0c06bd6086cbed9f9b107
andrecarlucci <andrecarlucci@gmail.com>
2017-09-29 15:50:37 -0300

    initial

---
+++ SuperTestFunction/HttpTrigger.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Primitives;
+using System;
+using System.Linq;
+
+namespace SuperTestFunction {
+    public class HttpTrigger {
+        public static IActionResult Run(HttpRequest req, TraceWriter log) {
+            log.Info("YESSSSSSSSSSSS!");
+
+            if (req.Query.TryGetValue("name", out StringValues value)) {
+                return new OkObjectResult($"Hello, {value.First()}");
+            }
+
+            return new BadRequestObjectResult("YESSSSSSSSSS!");
+        }
+    }
+}
